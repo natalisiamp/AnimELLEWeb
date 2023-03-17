d2a489ee4fb7ea7274c44fc12455abc3c5488473
Natali Siam-Pollo <nataliesiam@yahoo.com>
2023-03-16 22:11:55 -0400

    Refactored/Changed file names

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Animelle from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Animelle />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Animelle from './Animelle';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <App />
+    <Animelle />
 
   </React.StrictMode>
 );
